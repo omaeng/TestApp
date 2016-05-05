8df39850b0d08b1cd0ab254cbc3e3601bb8c20fc
오명훈 <myeonghuno@gmail.com>
2016-05-06 01:19:21 +0900

    2016년 05월 05일 Xamarin 튜토리얼을 통해 만들어본 예제입니다.

---
+++ TestApp/IDialer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TestApp
+{
+	public interface IDialer
+	{
+		bool Dial(string number);
+	}
+}
---
+++ iOS/PhoneDialer.cs
@@ -0,0 +1,20 @@
+﻿using Foundation;
+using TestApp.iOS;
+using System;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+
+namespace TestApp.iOS
+{
+	public class PhoneDialer : IDialer
+	{
+		public bool Dial(string number)
+		{
+			return UIApplication.SharedApplication.OpenUrl (
+				new NSUrl ("tel:" + number)
+			);
+		}
+	}
+}
